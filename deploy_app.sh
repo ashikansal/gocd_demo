1a1666035a64366030b7605995c1033f45329ac8
Ashi Kansal <ashi.kansal@INashikansal.local>
2020-05-30 16:45:02 +0530

    added script to remove old container:

---
+++ deploy_app.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#check if container exist with given name , delete it
+name=$1
+port=$2
+image=$3
+version=$4
+container_id=`docker ps -aqf name=$name`
+echo $container_id
+if [ -n $container_id ] ; then
+	docker rm $name -f
+fi
+
+# docker pull $image:$version
+docker run --rm -p $port:8080 --name $name -d $image:$version
